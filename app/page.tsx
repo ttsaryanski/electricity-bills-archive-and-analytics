dd9df339175ff9bc2986ac16326495f07a31e077
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-07-07 16:18:05 +0300

    Edit home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,16 @@ const HomePage = async () => {
             <div className="container mx-auto px-4 py-16">
                 <div className="text-center">
                     <h1 className="text-5xl font-bold text-gray-900 mb-6">
-                        Inventory Management
+                        Bills history and analytics
                     </h1>
                     <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
-                        Streamline your inventory tracking with our powerful,
-                        easy-to-use management system. Track products, monitor
-                        stock levels, and gain valuable insights.
+                        Optimize the management of your electricity bills with
+                        our powerful and easy-to-use system. Track your bills,
+                        monitor consumption and get valuable insights.
                     </p>
                     <div className="flex gap-4 justify-center">
-                        {/* <Link
-                            href="/sign-in"
-                            className="bg-purple-600 text-white px-8 py-3 rounded-lg font-semibold hover:bg-purple-700 transition-colors"
-                        >
-                            Sign In
-                        </Link> */}
                         <Link
-                            href="/learn-more"
+                            href="/dashboard"
                             className="bg-white text-purple-600 px-8 py-3 rounded-lg font-semibold border-2 border-purple-600 hover:bg-purple-50 transition-colors"
                         >
                             Learn More
